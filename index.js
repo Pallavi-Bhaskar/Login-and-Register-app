8779ced3d7528cb289052aa8a10e3304581295ab
Pallavi <101738151+Pallavi-Bhaskar@users.noreply.github.com>
2022-07-05 15:37:22 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,76 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
 
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cors());
 
-ReactDOM.createRoot(
-
-  
-    <App />,
-    document.getElementById('root')
-
-
+mongoose.connect(
+  "mongodb://localhost:27017/myLoginRegisterDB",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log("DB connected");
+  }
 )
+
+const userSchema = mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+});
+
+const User = new mongoose.model("User", userSchema);
+
+//Routes
+app.post("/login", (req, res) => {
+    const { email, password } = req.body
+    User.findOne({ email: email }, (err, user) => {
+        if(user){
+            if(password === user.password ){
+            res.send({message: "Login Successful", user: user})
+        }
+        else {
+            res.send({ message: "Password didn't match"})
+        }
+    }
+        
+        else {
+            res.send({message: "User not registered"})
+        }
+    })
+})
+
+app.post("/register", (req, res) => {
+ const { name, email, password} = req.body
+ User.findOne({email: email}, (err, user) => {
+    if(user){
+        res.send({message: "User already registered"})
+    }
+    else{
+        const user = new User({
+            name,
+            email,
+            password
+         })
+         user.save( err => {
+            if(err){
+                res.send(err)
+            }
+            else{
+                res.send({ message: "Successfully Registered, Please login now"})
+            }
+         })
+    }
+    
+ })
+ 
+})
+
+app.listen(9001, () => {
+  console.log("BE started at port 9001");
+})
